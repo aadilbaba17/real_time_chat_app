c2da31e8feba16736a132c4e859ef4c3cfdd2843
aadilbaba17 <abaadilbaba@gmail.com>
2024-08-12 00:52:14 +0530

    profile pic separartion

--- backend/controllers/auth.controller.js
+++ backend/controllers/auth.controller.js
@@ -16,7 +16,7 @@ export const signup= async(req,res) => {
         const salt = await bcryptjs.genSalt(10);
         const hashedPassword = await bcryptjs.hash(password, salt);
         const boyProfilePic = `https://avatar.iran.liara.run/public/boy?username=${username}`
-         const girlProfilePic = `https://avatar.iran.liara.run/public/boy?username=${username}`
+         const girlProfilePic = `https://avatar.iran.liara.run/public/girl?username=${username}`
 
     const newUser = new User({
         fullName,
