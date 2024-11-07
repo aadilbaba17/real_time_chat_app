81252a63f36b766a13af7699aa0553e65dbfea55
aadilbaba17 <abaadilbaba@gmail.com>
2024-11-07 23:02:56 +0530

    message vanish ISSUE

--- frontend/src/hooks/useListenMessages.jsx
+++ frontend/src/hooks/useListenMessages.jsx
@@ -12,25 +12,14 @@ const useListenMessages = () => {
      
 	useEffect(() => {
 		socket?.on("newMessage", (newMessage) => {
-		  // Play notification sound for the new message
-		  const sound = new Audio(notificationSound);
-		  sound.play();
-	
-		  // Only update the messages if the new message is part of the selected conversation
-		  if (selectedConversation?._id === newMessage?.conversationId) {
-			// Use functional update to get the latest state of messages
-			setMessages((prevMessages) => {
-			  // Prevent adding the same message multiple times
-			  if (!prevMessages.some((msg) => msg._id === newMessage._id)) {
-				return [...prevMessages, newMessage]; // Append the new message to the array
-			  }
-			  return prevMessages; // If the message already exists, return the previous state
-			});
-		  }
+			newMessage.shouldShake = true;
+			const sound = new Audio(notificationSound);
+			sound.play();
+			if( selectedConversation?._id == newMessage?.senderId ||selectedConversation?._id == newMessage?.receiverId){
+			setMessages([...messages, newMessage]);}
 		});
-	
-		return () => socket?.off("newMessage"); // Cleanup socket listener on unmount
-	  }, [socket, selectedConversation, setMessages]);
-	
-	};
+
+		return () => socket?.off("newMessage");
+	}, [socket, setMessages, messages]);
+};
 export default useListenMessages;
